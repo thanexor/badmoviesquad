02a2f0fc2bbd8538d8371b67f2dffec4bf244f13
Nathan Myers <nathanemyers@mgmail.com>
2020-06-27 22:34:20 -0500

    adding creds sorry

--- app/services/firebase.js
+++ app/services/firebase.js
@@ -1,12 +1,13 @@
 import * as firebase from 'firebase'
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDvM2q1mKESdbL3Bjea5Boph8OsOHBXDx0",
-  authDomain: "bms-admin-1337.firebaseapp.com",
-  databaseURL: "https://bms-admin-1337.firebaseio.com",
-  projectId: "bms-admin-1337",
-  storageBucket: "bms-admin-1337.appspot.com",
-  messagingSenderId: "42411448045"
+  apiKey: "AIzaSyB7XiBUzinKlCQ2ymSkVqzcPXcPyxx8364",
+  authDomain: "badmoviesquad-1337.firebaseapp.com",
+  databaseURL: "https://badmoviesquad-1337.firebaseio.com",
+  projectId: "badmoviesquad-1337",
+  storageBucket: "badmoviesquad-1337.appspot.com",
+  messagingSenderId: "304211569129",
+  appId: "1:304211569129:web:d7b896eedf7e95831ec378"
 })
 
 const db = firebase.firestore()
