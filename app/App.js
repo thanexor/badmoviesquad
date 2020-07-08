858676c2227887bd89dcdf28c50c214f9af27d5a
Jackson Mills <jackson.mills@steelseries.com>
2020-07-07 23:13:14 -0500

    Nav separate and site contained

--- app/App.js
+++ app/App.js
@@ -7,75 +7,55 @@ import {
   Link
 } from "react-router-dom"
 import { connect } from 'react-redux'
-
-import {
-  isLoggedIn,
-  getUsername,
-} from 'reduxState/selectors'
+import { isLoggedIn, getUsername } from "reduxState/selectors";
 
 import Nav from 'components/Nav'
-import NavItem from 'components/NavItem'
-import SignInButton from 'components/SignInButton'
-import Profile from 'components/Profile'
 
 import Home from 'containers/Home'
 import Scores from 'containers/Scores'
 import Upcoming from 'containers/Upcoming'
 import Backlog from 'containers/Backlog'
 
-const Container = styled.div`
+const Body = styled.div`
   font-family: 'Nunito Sans';
   background-color: ${({ theme }) => theme.neutral};
   min-height: 100vh;
 `
 
+const Container = styled.div`
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 15px;
+  padding-left: 15px;
+  max-width: 1540px;
+`
+
 function App(props) {
   return (
-    <Container>
+    <Body>
       <Router>
         <div>
-          <nav>
-            <Nav>
-              <NavItem>
-                <Link to="/">Home</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/scores">Scores</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/upcoming">Upcoming</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/backlog">Backlog</Link>
-              </NavItem>
-              <NavItem>
-                {props.isLoggedIn ?
-                  <Profile username={props.username} />
-                  :
-                  <SignInButton />
-                }
-              </NavItem>
-            </Nav>
-          </nav>
-
-          <Switch>
-            <Route path="/backlog">
-              <Backlog />
-            </Route>
-            <Route path="/upcoming">
-              <Upcoming />
-            </Route>
-            <Route path="/scores">
-              <Scores />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
+          <Nav username={ props.username } isLoggedIn={ props.isLoggedIn } />
+          <Container>
+            <Switch>
+              <Route path="/backlog">
+                <Backlog />
+              </Route>
+              <Route path="/upcoming">
+                <Upcoming />
+              </Route>
+              <Route path="/scores">
+                <Scores />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Container>
         </div>
       </Router>
-    </Container>
-  )
+    </Body>
+  );
 }
 
 export default connect(
