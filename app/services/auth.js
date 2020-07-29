a3b09eca136a7324f51826d45220234964a49677
Nathan Myers <nathanemyers@gmail.com>
2020-07-28 23:29:17 -0500

    fixing logout

--- app/services/auth.js
+++ app/services/auth.js
@@ -48,11 +48,8 @@ export function signIn() {
 
 export function signOut() {
   const provider = new firebase.auth.GoogleAuthProvider()
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
+  firebase.auth().signOut().then(function(result) {
+    console.log('signed out successfully')
   }).catch(function({ code, message, email, credential }) {
     console.error("Error signing in: ", message)
   }).finally(() => {
