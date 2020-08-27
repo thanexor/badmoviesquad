e211896e467d8ed621693c381edacc6f702752f5
Nathan Myers <nathanemyers@gmail.com>
2020-08-26 21:38:57 -0500

    i love michelle rodriguez

--- app/services/firebase.js
+++ app/services/firebase.js
@@ -1,13 +1,23 @@
 import * as firebase from 'firebase'
 
 firebase.initializeApp({
-  apiKey: "AIzaSyB7XiBUzinKlCQ2ymSkVqzcPXcPyxx8364",
-  authDomain: "badmoviesquad-1337.firebaseapp.com",
-  databaseURL: "https://badmoviesquad-1337.firebaseio.com",
-  projectId: "badmoviesquad-1337",
-  storageBucket: "badmoviesquad-1337.appspot.com",
-  messagingSenderId: "304211569129",
-  appId: "1:304211569129:web:d7b896eedf7e95831ec378"
+// new FIRESTORE (badmoviesquad)
+  //apiKey: "AIzaSyB7XiBUzinKlCQ2ymSkVqzcPXcPyxx8364",
+  //authDomain: "badmoviesquad-1337.firebaseapp.com",
+  //databaseURL: "https://badmoviesquad-1337.firebaseio.com",
+  //projectId: "badmoviesquad-1337",
+  //storageBucket: "badmoviesquad-1337.appspot.com",
+  //messagingSenderId: "304211569129",
+  //appId: "1:304211569129:web:d7b896eedf7e95831ec378",
+ 
+// old FIRESTORE (BMS-ADMIN)
+    apiKey: "AIzaSyDvM2q1mKESdbL3Bjea5Boph8OsOHBXDx0",
+    authDomain: "bms-admin-1337.firebaseapp.com",
+    databaseURL: "https://bms-admin-1337.firebaseio.com",
+    projectId: "bms-admin-1337",
+    storageBucket: "bms-admin-1337.appspot.com",
+    messagingSenderId: "42411448045",
+    appId: "1:42411448045:web:2ac841cd8b2912a8d61ee4"
 })
 
 export default firebase
